6f16121b294cbf0cc3828b6c9c302c280853a893
Zihao Shao <zhshao@yahoo.com>
2019-05-01 17:26:09 -0400

    php to make connection between html and database

---
+++ connect.php
@@ -0,0 +1,43 @@
+<?php
+
+	$username = filter_input(INPUT_POST, 'username');
+	$password = filter_input(INPUT_POST, 'password');
+	
+	if(!empty($username)){
+		if(!empty($password)){
+		$host = "localhost";
+		$dbusername = "root";
+		$dbpassword = "";
+		$dbname = "123456";
+		
+		
+		//create connection
+		$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+		
+		if(mysqli_connect_error()){
+			die('Connect Error('.my sqli_connect_errno().')'
+			.mysqli_connect_error());
+		}
+		else{
+			$sql="INSERT INTO account (username, password)
+			value ('username','$password')";
+			if ($conn->query($sql)){
+				echo "New record is inserted sucessfully";
+			}
+			else{
+				echo "Error: ". $sql. "<br>". $conn->error;
+			}
+			$conn->close();
+		}
+	}
+		
+		else{
+			echo "Password should not be empty";
+		    die();
+		}
+		 }
+		 else{
+		 	echo "Username should not be empty";
+		 	die();
+		 }
+?>

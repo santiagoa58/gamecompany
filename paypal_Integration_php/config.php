4b337d4791c4b3cf3a607208251aaba4c63eaea5
Zihao Shao <zhshao@yahoo.com>
2019-05-05 18:45:03 -0400

    Add files via upload

---
+++ paypal_Integration_php/config.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Basic Site Settings and API Configuration
+ */
+
+// Product details
+$itemName   = "Premium Game Membership Purchase";
+$itemNumber = "P123456";
+$payableAmount = 10;
+$currency   = "USD";
+
+ 
+// PayPal API configuration
+define('PAYPAL_API_USERNAME', 'API_Username');
+define('PAYPAL_API_PASSWORD', 'API_Password');
+define('PAYPAL_API_SIGNATURE', 'API_Signature');
+define('PAYPAL_SANDBOX', TRUE); //TRUE or FALSE
+
+// Database configuration
+define('DB_HOST', 'MySQL_Database_Host');
+define('DB_USERNAME', 'MySQL_Database_Username');
+define('DB_PASSWORD', 'MySQL_Database_Password');
+define('DB_NAME', 'MySQL_Database_Name');

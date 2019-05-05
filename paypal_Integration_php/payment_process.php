37168108a1f09dd7dffc7dd56a6f4370a1f66ba6
Zihao Shao <zhshao@yahoo.com>
2019-05-05 18:08:15 -0400

    Paypal payment php
     Helps to make credit card payment by PayPal Payments Pro
     
    In "config.php" file
    ===> Specify the database host (DB_HOST), username (DB_USERNAME), password (DB_PASSWORD), and name (DB_NAME) as per your MySQL database credentials.
    ===> Specify the API username (PAYPAL_API_USERNAME), password (PAYPAL_API_PASSWORD), and signature (PAYPAL_API_SIGNATURE) as per your PayPal API credentials.

---
+++ paypal_Integration_php/payment_process.php
@@ -0,0 +1,72 @@
+<?php
+
+// Include configuration file
+include_once 'config.php';
+
+// Include database connection file
+include_once 'dbConnect.php';
+
+// Include PayPalPro PHP library
+require_once 'PaypalPro.class.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    
+    // Buyer information
+	$name = $_POST['name_on_card'];
+	$nameArr = explode(' ', $name);
+    $firstName = !empty($nameArr[0])?$nameArr[0]:'';
+    $lastName = !empty($nameArr[1])?$nameArr[1]:'';
+    $city = 'Charleston';
+    $zipcode = '25301';
+    $countryCode = 'US';
+	
+	// Card details
+	$creditCardNumber = trim(str_replace(" ","",$_POST['card_number']));
+	$creditCardType = $_POST['card_type'];
+	$expMonth = $_POST['expiry_month'];
+	$expYear = $_POST['expiry_year'];
+	$cvv = $_POST['cvv'];
+    
+    // Create an instance of PaypalPro class
+	$paypal = new PaypalPro();
+	
+	// Payment details
+    $paypalParams = array(
+        'paymentAction' => 'Sale',
+		'itemName' => $itemName,
+		'itemNumber' => $itemNumber,
+        'amount' => $payableAmount,
+        'currencyCode' => $currency,
+        'creditCardType' => $creditCardType,
+        'creditCardNumber' => $creditCardNumber,
+        'expMonth' => $expMonth,
+        'expYear' => $expYear,
+        'cvv' => $cvv,
+        'firstName' => $firstName,
+        'lastName' => $lastName,
+        'city' => $city,
+        'zip'	=> $zipcode,
+        'countryCode' => $countryCode,
+    );
+    $response = $paypal->paypalCall($paypalParams);
+    $paymentStatus = strtoupper($response["ACK"]);
+    if($paymentStatus == "SUCCESS"){
+		// Transaction info
+		$transactionID = $response['TRANSACTIONID'];
+		$paidAmount = $response['AMT'];
+		
+		// Insert tansaction data into the database
+        $sql = "INSERT INTO orders(name,card_num,card_exp_month,card_exp_year,card_cvc,item_name,item_number,item_price,item_price_currency,paid_amount,paid_amount_currency,txn_id,payment_status,created,modified) VALUES('".$name."','".$creditCardNumber."','".$expMonth."','".$expYear."','".$cvv."','".$itemName."','".$itemNumber."','".$payableAmount."','".$currency."','".$paidAmount."','".$currency."','".$transactionID."','".$paymentStatus."',NOW(),NOW())";
+        $insert = $db->query($sql);
+        $last_insert_id = $db->insert_id;
+		
+		$data['status'] = 1;
+        $data['orderID'] = $last_insert_id;
+    }else{
+         $data['status'] = 0;
+    }
+	
+	// Transaction status
+    echo json_encode($data);
+}
+?>
